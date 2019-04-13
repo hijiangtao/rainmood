7ef059c178c924ed76e7ccaaaad5ff3179d09707
hijiangtao <hijiangtao@gmail.com>
2019-04-13 21:59:35 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const NUMBER_CONST = '0123456789';
+const REPO_GITHUB = "https://github.com/hijiangtao/rainmood";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={process.env.PUBLIC_URL + '/favicon.png'} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            倾听 专注  
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <span style={{
+            color: '#666',
+            fontSize: '0.8rem',
+          }}>
+            循环播放十首电影原声精选，背景乐为下雨声。
+            <br/>
+            详见&nbsp;
+            <a
+              className="App-link"
+              href={REPO_GITHUB}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+          </span>
         </header>
+
+        <footer>
+          <audio autoPlay loop>
+            <source src={process.env.PUBLIC_URL + "/music/main.m4a"} />
+          </audio>
+
+          <audio autoPlay loop>
+            {NUMBER_CONST.split('').map((e, i) => {
+              return (
+                <source key={i} alt={`music-${i}`} src={`${process.env.PUBLIC_URL}/music/${i}.mp3`} />
+              )
+            })}  
+          </audio>
+        </footer>
       </div>
     );
   }
