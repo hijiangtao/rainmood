48e4c44189651fdb7a02af65e323faa9d9b7b106
hijiangtao <hijiangtao@gmail.com>
2019-04-13 22:20:40 +0800

    gh-pages init commit

---
+++ precache-manifest.5bf30f1bcf8840fe61e8ec55b7fba624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7c98764185a0b5ba4b8",
+    "url": "/rainmood/static/js/runtime~main.85f740b5.js"
+  },
+  {
+    "revision": "43cd13acfd8f1de16f45",
+    "url": "/rainmood/static/js/main.2531e44e.chunk.js"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/rainmood/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "43cd13acfd8f1de16f45",
+    "url": "/rainmood/static/css/main.99e58424.chunk.css"
+  },
+  {
+    "revision": "595f7b65416de3404993f9d0909f863f",
+    "url": "/rainmood/index.html"
+  }
+];
